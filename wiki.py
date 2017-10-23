e7b496bdf1d175295a13fdb90075dc7932a9eb8c
Dickon Reed <dickon@cantab.net>
2017-10-23 16:00:55 +0100

    green bar: saved and re-read files
    also cleanup after the end of the test

--- wiki.py
+++ wiki.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from json import dumps
-
+from os.path import join
 app = Flask(__name__)
 
 @app.route("/documents")
@@ -8,6 +8,16 @@ def documents():
     return dumps([])
 
 @app.route("/documents/<name>", methods=['POST'])
-def page(name):
-    return dumps({'content':''})
+def post_page(name):
+    # TODO: validate name
+    with open(join(app.config['ROOT'], name), 'wb') as f:
+        f.write(request.data)
+        return 'saved'
 
+@app.route("/documents/<name>/latest", methods=['GET'])
+def get_latest_page(name):
+    # TODO: validate name
+    with open(join(app.config['ROOT'], name), 'r') as f:
+        return dumps({'content':f.read()})
+
+        
--- wiki_tests.py
+++ wiki_tests.py
@@ -1,13 +1,19 @@
 from unittest import TestCase, main
 from wiki import app
 from os import close, unlink
+from tempfile import mkdtemp
 from json import loads
+from shutil import rmtree
 
 class WikiTestCase(TestCase):
     def setUp(self):
         app.testing = True
+        app.config['ROOT'] = mkdtemp()
         self.app = app.test_client()
-            
+        
+    def tearDown(self):
+        rmtree(app.config['ROOT'])
+        
     def test_empty(self):
         rv = self.app.get('/documents')
         assert rv.data == b"[]"
@@ -17,6 +23,10 @@ class WikiTestCase(TestCase):
     def test_single_page(self):
         rv = self.app.post('/documents/test', data='hello world')
         assert rv.status_code == 200
-
+        rv2 = self.app.get('/documents/test/latest')
+        out2 = loads(rv2.data.decode())
+        assert out2['content'] == 'hello world'
+    
+        
 if __name__ == '__main__':
     main()
