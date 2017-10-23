e435a34f940259e2a1abbe1a99fcac6f2e3bbfca
Dickon Reed <dickon@cantab.net>
2017-10-23 15:30:05 +0100

    green bar: get test working
    (got caught out by the test case getting a response value rather than simply the data)

--- wiki_tests.py
+++ wiki_tests.py
@@ -1,10 +1,18 @@
-from unittest import TestCase
+from unittest import TestCase, main
 from wiki import app
+from tempfile import mkstemp
+from os import close, unlink
 
 class WikiTestCase(TestCase):
     def setUp(self):
         app.testing = True
+        self.app = app.test_client()
+            
     def test_empty(self):
         rv = self.app.get('/documents')
-        assert rv == []
-
+        print(rv.data)
+        assert rv.data == b"[]"
+        pass
+    
+if __name__ == '__main__':
+    main()
